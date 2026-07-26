61c2890e0902b8a06dbe221b65a1e25343d6abac
Pranay Narain <pranay.23b0101281@abes.ac.in>
2026-07-26 18:54:14 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (80.15%) - LeetSync

---
+++ Array/91-decode-ways/decode-ways.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> dp;
+    int solve(int i, string& s) {
+        if (i == s.size())
+            return 1;
+
+        if (s[i] == '0')
+            return 0;
+
+        if (dp[i] != -1)
+            return dp[i];
+
+        int take = 0;
+        int notTake = 0;
+
+        take = solve(i + 1, s);
+
+        if (i + 1 < s.size() &&
+            (s[i] == '1' || (s[i] == '2' && s[i + 1] <= '6'))) {
+
+            notTake = solve(i + 2, s);
+        }
+
+          return dp[i] = take + notTake;
+    }
+
+    int numDecodings(string s){
+         dp.resize(s.size(), -1);
+         return solve(0, s);
+      }
+};
