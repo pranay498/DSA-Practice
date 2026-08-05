74f1612ec78935d2cbbe44e2c2a22d91bf039f4d
Pranay Narain <pranay.23b0101281@abes.ac.in>
2026-08-05 19:38:19 +0530

    Time: 450 ms (25.35%) | Memory: 41.6 MB (35.23%) - LeetSync

--- Array/300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ Array/300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,21 +1,28 @@
 class Solution {
-public:
-    int lengthOfLIS(vector<int>& nums) {
-        if (nums.empty()) {
-            return 0;
+public: 
+     int dp[2501][2501];
+    int solve(int idx, int prev, vector<int>& nums) {
+
+        if(idx>=nums.size())
+        return 0 ;
+
+         if (dp[idx][prev + 1] != -1)
+            return dp[idx][prev + 1];
+
+        int take = 0;
+
+        if (prev == -1 || nums[prev] < nums[idx]) 
+        {
+         take = 1 + solve(idx + 1, idx , nums);
         }
+        int not_take = solve(idx + 1, prev, nums);
 
-        int n = nums.size();
-        std::vector<int> dp(n, 1);
+        return dp[idx][prev + 1] = max(take, not_take);
+    }
 
-        for (int i = 1; i < n; ++i) {
-            for (int j = 0; j < i; ++j) {
-                if (nums[i] > nums[j]) {
-                    dp[i] = std::max(dp[i], dp[j] + 1);
-                }
-            }
-        }
-
-        return *std::max_element(dp.begin(), dp.end());
+    int lengthOfLIS(vector<int>& nums)
+     {
+         memset(dp, -1, sizeof(dp));
+         return solve(0, -1, nums);
     }
 };
