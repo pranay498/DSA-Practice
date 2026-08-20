c93cd728894e0a19ef1a0b40091a5d3a2e213580
Pranay Narain <pranay.23b0101281@abes.ac.in>
2026-08-20 23:44:54 +0530

    Time: 7 ms (5.50%) | Memory: 12.3 MB (5.27%) - LeetSync

---
+++ Array/2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+       string ans = "";
+        int n = part.size();
+
+        for(char c : s) {
+            ans.push_back(c);
+
+            if(ans.size() >= n && 
+               ans.substr(ans.size() - n) == part) {
+                ans.erase(ans.size() - n);
+            }
+        }
+
+        return ans; 
+    }
+};
