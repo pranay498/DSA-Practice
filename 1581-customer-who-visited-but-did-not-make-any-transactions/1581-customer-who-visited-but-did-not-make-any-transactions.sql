b0079835749a25701af30be74180b658b2d350e6
Pranay Narain <pranay.23b0101281@abes.ac.in>
2026-08-21 16:09:24 +0530

    Time: 1630 ms (33.02%), Space: 0B (100%) - LeetHub

---
+++ 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select v.customer_id ,count(*) count_no_trans 
+from visits v
+left join transactions t
+on v.visit_id = t.visit_id 
+where t.transaction_id is NULL
+group by v.customer_id
+order by count_no_trans desc
