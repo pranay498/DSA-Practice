7f84cb6a3aacab5456fc882f8966246dfb752091
Pranay Narain <pranay.23b0101281@abes.ac.in>
2026-09-10 01:08:05 +0530

    Time: 67 ms (20.84%), Space: 167.8 MB (18.67%) - LeetHub

--- 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.cpp
+++ 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.cpp
@@ -1,26 +1,19 @@
 class Solution {
 public:
     bool mergeTriplets(vector<vector<int>>& triplets, vector<int>& target) {
-        int n = target.size();
-        vector<int> ans(n);
-
-        if (triplets.size() == 1) {
-            ans[0] = triplets[0][0];
-            ans[1] = triplets[0][1];
-            ans[2] = triplets[0][2];
-
-            return ans == target;
+        bool a=false;
+        bool b=false;
+        bool c=false;
+        for(auto t:triplets){
+            if(t[0]>target[0]||t[1]>target[1]||t[2]>target[2])
+            continue;
+            if(t[0]==target[0])
+            a=true;
+            if(t[1]==target[1])
+            b=true;
+            if(t[2]==target[2])
+            c=true;
         }
-
-        for (auto& triplet : triplets) {
-            if (triplet[0] <= target[0] && triplet[1] <= target[1] &&
-                triplet[2] <= target[2]) {
-                ans[0] = max(ans[0], triplet[0]);
-                ans[1] = max(ans[1], triplet[1]);
-                ans[2] = max(ans[2], triplet[2]);
-            }
-        }
-
-        return ans == target;
+        return a && b && c;
     }
 };
